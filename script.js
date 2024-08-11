38090d8310843c48d87d7c5a5eba311779068612
Lavanyaraju29 <slavanya2901@gmail.com>
2024-08-11 22:34:03 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.getElementById('saveButton').addEventListener('click', function() {
     const noteInput = document.getElementById('noteInput');
     const noteText = noteInput.value.trim();
@@ -22,30 +21,4 @@ document.getElementById('saveButton').addEventListener('click', function() {
 function deleteNote(button) {
     const notesList = document.getElementById('notesList');
     notesList.removeChild(button.parentElement);
-=======
-document.getElementById('addTaskBtn').addEventListener('click', addTask);
-
-function addTask() {
-    const taskInput = document.getElementById('taskInput');
-    const taskText = taskInput.Value.trim();
-
-    if (taskText !== "") {
-        const taskList = document.getElementById('tasklist');
-
-    const li = document.getElementById('li');
-    li.notecontent = taskText;
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.className = "deleteBtn";
-    deleteBtn.addEventListener("click", function(){
-        noteList.removeChild(li);
-    });
-
-    li.appendChild(deleteBtn);
-    noteList.appendChild(li);
-
-    noteInput.value = "";
-    } 
->>>>>>> 3c1747c982bf100e5e2e00f379dcaf32fa986f46
 }
